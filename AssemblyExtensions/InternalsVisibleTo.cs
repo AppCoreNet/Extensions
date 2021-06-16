56041005291f4b76e709fd8c2859023423f8c111
Christian Prochnow <christian.prochnow77@gmail.com>
2021-06-16 09:25:40 +0200

    Renaming.

--- AssemblyExtensions/InternalsVisibleTo.cs
+++ AssemblyExtensions/InternalsVisibleTo.cs
@@ -3,4 +3,4 @@
 
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("AppCore.Extensions.DependencyInjection.AssemblyExtensions.Tests")]
+[assembly: InternalsVisibleTo("AppCore.DependencyInjection.AssemblyExtensions.Tests")]
