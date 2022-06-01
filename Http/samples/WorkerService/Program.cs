504c10f6ef1227044f088fa31d691bbec01d5e60
Christian Prochnow <christian.prochnow77@gmail.com>
2022-06-01 15:58:45 +0200

    Added WorkerService sample for AppCore.Extensions.Http.Authentication

---
+++ Http/samples/WorkerService/Program.cs
@@ -0,0 +1,34 @@
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using WorkerService;
+
+IHost host =
+    Host.CreateDefaultBuilder(args)
+        .ConfigureServices(
+            services =>
+            {
+                services.AddHttpClientAuthentication()
+                        .AddOAuthClient(
+                            "client",
+                            o =>
+                            {
+                                o.TokenEndpoint = new Uri("https://demo.duendesoftware.com/connect/token");
+                                o.ClientId = "m2m.short";
+                                o.ClientSecret = "secret";
+                                o.Scope = "api";
+                            });
+
+                services.AddHttpClient(
+                            "api-client",
+                            client =>
+                            {
+                                client.BaseAddress = new Uri("https://demo.duendesoftware.com/api/");
+                            })
+                        .AddOAuthClientAuthentication("client");
+
+                services.AddHostedService<Worker>();
+            })
+        .Build();
+
+await host.RunAsync();
---
+++ Http/samples/WorkerService/Worker.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace WorkerService;
+
+public class Worker : BackgroundService
+{
+    private readonly ILogger<Worker> _logger;
+    private readonly IHttpClientFactory _factory;
+
+    public Worker(ILogger<Worker> logger, IHttpClientFactory factory)
+    {
+        _logger = logger;
+        _factory = factory;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        while (!stoppingToken.IsCancellationRequested)
+        {
+            _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+
+            HttpClient client = _factory.CreateClient("api-client");
+            HttpResponseMessage response = await client.GetAsync("test", stoppingToken);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync(stoppingToken);
+                _logger.LogInformation("API response: {response}", content);
+            }
+            else
+            {
+                _logger.LogError("API returned: {statusCode}", response.StatusCode);
+            }
+
+            await Task.Delay(5000, stoppingToken);
+        }
+    }
+}
