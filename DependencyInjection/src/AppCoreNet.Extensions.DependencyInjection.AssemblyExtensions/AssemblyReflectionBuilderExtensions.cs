a8634eeea8e548f620f9d8cf3e84507ef03789fe
Christian Prochnow <christian.prochnow77@gmail.com>
2025-05-22 18:16:39 +0200

    Updated AOT attributes.

--- DependencyInjection/src/AppCoreNet.Extensions.DependencyInjection.AssemblyExtensions/AssemblyReflectionBuilderExtensions.cs
+++ DependencyInjection/src/AppCoreNet.Extensions.DependencyInjection.AssemblyExtensions/AssemblyReflectionBuilderExtensions.cs
@@ -11,7 +11,7 @@ namespace AppCoreNet.Extensions.DependencyInjection;
 /// <summary>
 /// Provides extension methods to resolve service descriptors and facilities by reflection.
 /// </summary>
-[RequiresUnreferencedCode("Uses reflection to discover services.")]
+[RequiresUnreferencedCode("Uses reflection to discover types.")]
 public static class AssemblyReflectionBuilderExtensions
 {
     /// <summary>
